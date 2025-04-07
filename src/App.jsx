ce75808896264c0b99f38914cbf5656fec724dc4
GlebZemlyanikin <zemlyanikin.g.96@mail.ru>
2025-04-07 21:09:52 +0300

    это js

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { createElement } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-  const year = new Date().getFullYear() // Декларативный стиль
+  const year = new Date().getFullYear()
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-	  <div>{year}</div>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button> // Императивный стиль
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  return createElement(
+		React.Fragment, null,
+		createElement('h1', null, 'Vite + React'),
+		createElement('div', null, year)
   )
 }
-
-/* <button onClick={() => setCount((count) => count + 1)}>
-count is {count}
-</button> // Императивный стиль */
 
 export default App
